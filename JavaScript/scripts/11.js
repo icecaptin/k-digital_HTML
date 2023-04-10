2b93e3cada72c4a823a4f2bbc989fdcd59dd1a8e
Icecaptin <tjdqn0228@gmail.com>
2023-04-10 23:04:44 +0900

    20230410_AtHome

---
+++ JavaScript/scripts/11.js
@@ -0,0 +1,8 @@
+function show() {
+  //   alert("TA-DA!");
+  document.getElementById("h2_id").innerHTML = "TA-DA!";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("h2_id").innerHTML = "TA-DA!";
+});
